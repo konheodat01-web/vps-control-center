96028cc47129c07bb7c833332b90f43cd5426338
konheodat01-web <konheodat01@gmail.com>
2026-05-08 12:46:22 +0700

    fix: bump SW cache version v1->v2 to force clear old cached app.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vps-control-v1';
+const CACHE_NAME = 'vps-control-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
